6264a5e3d94088af21c211ff1618a2578a8da947
Najman <najman.husaini@utah.edu>
2023-04-09 01:26:17 -0600

    update logo

--- static/index.service.worker.js
+++ static/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1680987218|3832054497";
+const CACHE_VERSION = "1681025150|56976949";
 const CACHE_PREFIX = "Bola-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
